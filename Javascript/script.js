712d169b58ed1fe57bd247f43a2662b873aa99d8
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-04-14 14:31:13 +0200

    added Serris changes

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,23 +1,40 @@
 let movieData = JSON.parse(movies);
-console.log(movieData);
+// console.log(movieData);
 
-function generateHTML(){
-  let cardGenerator = "";
-  for (i in movieData.Movies){
+function generateHTML(array){
+  let cardGenerator = ``;
+  for (i in array){
     cardGenerator += `
     <div id="movieCard" class="card bg-dark mb-3" style="max-width: 540px;">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="${movieData.Movies[i].image}" alt="movie image failed to load">
+          <img src="${array[i].image}" alt="movie image failed to load">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-              <h5 id="movieTitle" class="card-title text-white">${movieData.Movies[i].movieTitle}</h5>
-              <p class="card-text text-white">${movieData.Movies[i].description}</p>
-              <p class="card-text text-danger">${movieData.Movies[i].genre}</p>
-              <span id="likeValue" class="bg-success text-white rounded p-1">${movieData.Movies[i].likes}</span><br><br>
+              <h5 id="movieTitle" class="card-title text-white">${array[i].movieTitle}</h5>
+              <p class="card-text text-white">${array[i].description}</p>
+              <p class="card-text text-danger">${array[i].genre}</p>
+              <span id="likeValue" class="bg-success text-white rounded p-1">${array[i].likes}</span><br><br>
               <p id="likeBtn" class="btn btn-success like">Did You like this movie?</p>
           </div>
+        </div>
+
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="title-tab${i}" data-bs-toggle="tab" data-bs-target="#title${i}" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="description-tab${i}" data-bs-toggle="tab" data-bs-target="#description${i}" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="genre-tab${i}" data-bs-toggle="tab" data-bs-target="#genre${i}" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
+          </li>
+        </ul>
+        <div class="tab-content text-warning" id="myTabContent">
+          <div class="tab-pane fade show active" id="title${i}" role="tabpanel" aria-labelledby="home-tab">${array[i].movieTitle}</div>
+          <div class="tab-pane fade" id="description${i}" role="tabpanel" aria-labelledby="profile-tab">${array[i].description}</div>
+          <div class="tab-pane fade" id="genre${i}" role="tabpanel" aria-labelledby="contact-tab">${array[i].genre}</div>
         </div>
       </div>
     </div>
@@ -25,31 +42,45 @@ function generateHTML(){
 }
 return cardGenerator
 }
-document.getElementById("content").innerHTML += generateHTML();
+
+document.getElementById("content").innerHTML += generateHTML(movieData.Movies);
 
 let likeBtns = document.querySelectorAll("#likeBtn");
 let sortBtn = document.querySelector("#sortBtn");
 
-likeBtns.forEach(function(currentBtn){ // we take the button #like, the forEach loops goes over and attaches an event listener to each #like 
+likeBtns.forEach(function(currentBtn, index){ // we take the button #like, the forEach loops goes over and attaches an event listener to each #like 
   currentBtn.addEventListener("click", function(){
       console.log(this.parentNode.querySelector("#likeValue")); //.this in an eventlistener refers to the element the event is called on. SO when we call this on the ForEach button, we get access to all btns as individuals
-      let likedValue = this.parentNode.querySelector("#likeValue").innerHTML; //then declare a variable that will go into the parentNode of the pressed button and access teh innerHTML of the #likeValue
+      let likedValue = movieData.Movies[index].likes ++; //then declare a variable that will go into the parentNode of the pressed button and access teh innerHTML of the #likeValue
       // console.log(typeof likedValue);
-      likedValue = parseInt(likedValue); //return it as an int because JSON. Need to regrab the the number of likes, and make that string = int + 1 every time eventlistener is called
-      this.parentNode.querySelector("#likeValue").innerHTML = likedValue + 1;
+      likedValue = parseInt(likedValue + 1); //return it as an int because JSON. Need to regrab the the number of likes, and make that string = int + 1 every time eventlistener is called
+      this.parentNode.querySelector("#likeValue").innerHTML = likedValue;
   })
 })
 
-sortBtn.addEventListener("click", sortFunction);
+sortBtn.addEventListener("click", function(){sortFunction(movieData.Movies)});
 
 
-function sortFunction(){
- let currentLikes = document.querySelectorAll("#likeValue"); //gather the items in a nodelist.
- currentLikes = Object.entries(currentLikes); //put them into an array.
- for (i = 0; i < currentLikes.length; i++){ //iterate through them
-   var likesArray = currentLikes[i];
-   console.log(likesArray[1]); //log the 1st value of the array.? I think.
-  //  I cant seem to make the leap in logic to then process the items the sort button is writing in the console.log
- }
-//  console.log(likesArray[1])
+function sortFunction(arr){
+  // console.table(x);
+//  let currentLikes = document.querySelectorAll("#likeValue"); //gather the items in a nodelist.
+// //  currentLikes = Object.entries(currentLikes);
+// //  console.log(currentLikes) //put them into an array.
+//  for (i = 0; i < currentLikes.length; i++){ //iterate through them
+//   var sortedArray = currentLikes[i];
+//   let John = sortedArray.innerHTML
+//    console.log(John)
+//  }
+document.getElementById("content").innerHTML = "";
+console.table(arr);
+arr.sort((a,b)=> b.likes - a.likes);
+console.table(arr);
+document.getElementById("content").innerHTML += generateHTML(arr); 
 }
+
+// function sortFunction(){
+//   movieData.sort((a, b) => b.likes - a.likes);
+//   document.getElementById("content").innerHTML += generateHTML();
+// }
+
+
